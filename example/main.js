ec0dfc0c6e9edabc965c1313a13f7fdf8121e541
Samuel C Tyler <skamansam@gmail.com>
2022-08-12 14:21:13 -0400

    update highlight js vue component to use official package

--- example/main.js
+++ example/main.js
@@ -1,10 +1,11 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
-import VueHighlightJS from "vue-highlight.js";
+import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 import javascript from "highlight.js/lib/languages/javascript";
-import vue from "vue-highlight.js/lib/languages/vue";
+// import vue from "vue-highlight.js/lib/languages/vue";
 import css from "highlight.js/lib/languages/css";
+
 import VueDraggableResizable from "vue-draggable-resizable";
 import { Plotly } from "@/index.js";
 
@@ -14,19 +15,20 @@ import "font-awesome/css/font-awesome.css";
 import "highlight.js/styles/default.css";
 import "highlight.js/styles/mono-blue.css";
 
-Vue.component("vue-draggable-resizable", VueDraggableResizable);
-Vue.component("plotly", Plotly);
+const app = createApp({
+  render: h => h(App)
+}).$mount("#app");
 
-Vue.use(VueHighlightJS, {
+app.component("vue-draggable-resizable", VueDraggableResizable);
+app.component("plotly", Plotly);
+
+app.use(hljsVuePlugin, {
   languages: {
     css,
     javascript,
-    vue
+    // vue
   }
 });
 
-Vue.config.productionTip = false;
+app.mount('#app')
 
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
