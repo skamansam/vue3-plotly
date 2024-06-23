74847ca80de6fe884bda2ff0b5e7de21fc6c5dbd
Samuel C. Tyler <skamansam@gmail.com>
2024-06-23 16:52:47 -0400

    update highlightjs to the latest version (less deps!)

--- example/main.js
+++ example/main.js
@@ -2,31 +2,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 
-import javascript from "highlight.js/lib/languages/javascript";
-// import vue from "vue-highlight.js/lib/languages/vue";
-import css from "highlight.js/lib/languages/css";
-
 import VueDraggableResizable from "vue-draggable-resizable";
 import { Plotly } from "@/index.js";
 
-import "vue-draggable-resizable/dist/VueDraggableResizable.css";
+// import "vue-draggable-resizable/dist/vue-draggable-resizable.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
-import "highlight.js/styles/default.css";
-import "highlight.js/styles/mono-blue.css";
 
 const app = createApp(App);
 
-app.component("vue-draggable-resizable", VueDraggableResizable);
-app.component("plotly", Plotly);
+app.component("VueDraggableResizable", VueDraggableResizable);
+app.component("Plotly", Plotly);
 
-app.use(hljsVuePlugin, {
-  languages: {
-    css,
-    javascript,
-    // vue
-  }
-});
+app.use(hljsVuePlugin);
 
 app.mount('#app')
 
